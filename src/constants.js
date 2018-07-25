228979b7eb55dde231055e7dacff3aa2dd208945
Batista Harahap <batista@bango29.com>
2018-07-25 17:36:00 +0700

    add constants

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export default {
+  indodaxBaseUrl: 'https://indodax.com',
+  indodaxApiBaseUrl: `${this.indodaxBaseUrl}/api`,
+  indodaxTapiBaseUrl: `${this.indodaxBaseUrl}/tapi`,
+  indodaxTradingViewBaseUrl: `${this.indodaxTradingViewBaseUrl}/tradingview`,
+  defaultUserAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36',
+}
