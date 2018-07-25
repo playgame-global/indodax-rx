0d97c010cd31937bccdcead9508a0148c4d2ee06
Batista Harahap <batista@bango29.com>
2018-07-25 19:02:10 +0700

    added examples

--- src/constants.js
+++ src/constants.js
@@ -4,4 +4,5 @@ export default {
   indodaxTapiBaseUrl: `${this.indodaxBaseUrl}/tapi`,
   indodaxTradingViewBaseUrl: `${this.indodaxTradingViewBaseUrl}/tradingview`,
   defaultUserAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36',
+  websocketUrl: 'wss://ws-ap1.pusher.com/app/a0dfa181b1248b929b11?protocol=7&client=js&version=4.1.0&flash=false',
 }
